560fa30e2c1361b108e472093422de22ba278027
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2024-02-15 15:21:11 -0300

    refactor: added context to interfaces

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -1,14 +1,17 @@
 package ports
 
-import "github.com/viniciusgferreira/ps-tag-onboarding-go/internal/core/domain/models"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/viniciusgferreira/ps-tag-onboarding-go/internal/core/domain/models"
+)
 
 type UserService interface {
-	Find(id string) (models.User, error)
-	Save(u models.User) (models.User, error)
+	Find(ctx *gin.Context, id string) (*models.User, error)
+	Save(ctx *gin.Context, u *models.User) (*models.User, error)
 }
 
 type UserRepo interface {
-	FindById(id string) (models.User, error)
-	FindAll() ([]models.User, error)
-	Save(u models.User) (models.User, error)
+	FindById(ctx *gin.Context, id string) (*models.User, error)
+	FindAll(ctx *gin.Context) ([]*models.User, error)
+	Save(ctx *gin.Context, u *models.User) (*models.User, error)
 }
