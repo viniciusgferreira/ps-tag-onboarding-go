eedb9031214a8cfe2c57e2bd44f5ac577786f244
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2024-02-15 15:19:20 -0300

    refactor: json properties for User

--- internal/core/domain/models/user.go
+++ internal/core/domain/models/user.go
@@ -7,32 +7,27 @@ import (
 )
 
 type User struct {
-	id        string
-	firstName string
-	lastName  string
-	email     string
-	age       int
+	ID        string `json:"id,omitempty"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
 }
 
 func NewUser(id, firstName, lastName, email string, age int) User {
 	user := User{
-		id:        id,
-		firstName: firstName,
-		lastName:  lastName,
-		email:     email,
-		age:       age,
+		ID:        id,
+		FirstName: firstName,
+		LastName:  lastName,
+		Email:     email,
+		Age:       age,
 	}
 	return user
 }
-func (u *User) validate() error {
-	if err := u.validateID(); err != nil {
-		return err
-	}
-
+func (u *User) Validate() error {
 	if err := u.validateName(); err != nil {
 		return err
 	}
-
 	if err := u.validateEmail(); err != nil {
 		return err
 	}
@@ -43,32 +38,35 @@ func (u *User) validate() error {
 }
 
 func (u *User) validateID() error {
-	if len(u.id) <= 0 {
+	if len(u.ID) <= 0 {
 		return errors.New("id cannot be empty")
 	}
-	if err := uuid.Validate(u.id); err != nil {
+	if err := uuid.Validate(u.ID); err != nil {
 		return errors.New("invalid id")
 	}
 	return nil
 }
 
 func (u *User) validateName() error {
-	if len(u.firstName) <= 0 || len(u.lastName) <= 0 {
+	if len(u.FirstName) <= 0 || len(u.LastName) <= 0 {
 		return errors.New("first and last name cannot be empty")
 	}
 	return nil
 }
 
 func (u *User) validateEmail() error {
+	if len(u.Email) <= 0 {
+		return errors.New("email cannot be empty")
+	}
 	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
-	if !emailRegex.MatchString(u.email) {
+	if !emailRegex.MatchString(u.Email) {
 		return errors.New("invalid email format")
 	}
 	return nil
 }
 
 func (u *User) validateAge() error {
-	if u.age < 18 {
+	if u.Age < 18 {
 		return errors.New("user must be at least 18 years old")
 	}
 	return nil
