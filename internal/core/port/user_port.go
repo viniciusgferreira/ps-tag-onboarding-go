8bf0555d1b1c24d48d515b6968de64257a21bf63
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2024-03-11 12:13:41 -0300

    refactor: remove error return of repository interface method ExistsByFirstNameAndLastName

--- internal/core/port/user_port.go
+++ internal/core/port/user_port.go
@@ -5,15 +5,17 @@ import (
 	"github.com/viniciusgferreira/ps-tag-onboarding-go/internal/core/domain/model"
 )
 
+// remove interface
 type UserService interface {
 	Find(ctx *gin.Context, id string) (*model.User, error)
 	Save(ctx *gin.Context, u model.User) (*model.User, error)
 	Update(ctx *gin.Context, u model.User) (*model.User, error)
 }
 
+// move to service
 type UserRepository interface {
 	FindById(ctx *gin.Context, id string) (*model.User, error)
 	Save(ctx *gin.Context, u model.User) (*model.User, error)
 	Update(ctx *gin.Context, u model.User) (*model.User, error)
-	ExistsByFirstNameAndLastName(ctx *gin.Context, firstName, lastName string) (bool, error)
+	ExistsByFirstNameAndLastName(ctx *gin.Context, u model.User) bool
 }
