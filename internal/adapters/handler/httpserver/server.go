e1ad2f218b389d8ed69f4419f4e977f096c1b98b
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2024-04-03 14:02:58 -0300

    refactor: move router setup to NewServer function

--- internal/adapters/handler/httpserver/server.go
+++ internal/adapters/handler/httpserver/server.go
@@ -1,13 +1,31 @@
 package httpserver
 
 import (
+	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/viniciusgferreira/ps-tag-onboarding-go/internal/adapters/config"
 	"net/http"
 )
+
+type Router struct {
+	*gin.Engine
+}
 
 func NewServer(cfg *config.HTTP, serverHandlers []HttpHandlers) *http.Server {
 	return &http.Server{
 		Addr:    cfg.URL + ":" + cfg.Port,
 		Handler: NewRouter(cfg.GinMode, serverHandlers),
 	}
+}
+
+func NewRouter(ginMode string, handlers []HttpHandlers) *Router {
+	gin.SetMode(ginMode)
+	router := &Router{gin.Default()}
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	for _, handler := range handlers {
+		handler.SetupRoutes(router)
+	}
+	return router
 }
