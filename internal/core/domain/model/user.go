acc33cbbdaf4f6ef9ef9d899dddce799d82f75a6
Vinicius Ferreira <vinicius.gomesferreira@wexinc.com>
2024-04-11 16:44:51 -0300

    feat: newUSer from dto

--- internal/core/domain/model/user.go
+++ internal/core/domain/model/user.go
@@ -1,9 +1,20 @@
 package model
 
+import "github.com/viniciusgferreira/ps-tag-onboarding-go/internal/adapters/handler/dto"
+
 type User struct {
 	ID        string `bson:"_id,omitempty" json:"id,omitempty"`
 	FirstName string `bson:"firstName" json:"firstName"`
 	LastName  string `bson:"lastName" json:"lastName"`
 	Email     string `bson:"email" json:"email"`
 	Age       int    `bson:"age" json:"age"`
+}
+
+func NewUser(dto dto.UserInput) User {
+	return User{
+		FirstName: dto.FirstName,
+		LastName:  dto.LastName,
+		Email:     dto.Email,
+		Age:       dto.Age,
+	}
 }
